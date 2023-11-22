e1d67ca033a22ea182bbd68b3dde1b7375255230
rohit kumar <rohit.rk308@gmail.com>
2023-11-22 16:04:37 +0530

    Initial commit

---
+++ src/components/product_details/Loading.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Box,Image,Flex, Heading} from '@chakra-ui/react'
+
+const Loading = () => {
+     
+      return (
+         
+          
+            <Box  h='600px'>
+              <Image w='100%' h='100%' m='auto'/>
+               {/* <Heading  fontSize='100px' letterSpacing='2px' color='blue' textAlign='center'>Thank You From Domino'S Team</Heading> */}
+            </Box>
+            
+           
+           
+         
+      )
+    }
+
+export default Loading
